dfda3777ffb556810c3b2cfb24d2cdd38f1a63ad
Dasihub <dasihub02@gmail.com>
2022-01-18 10:23:58 +0600

    ffd

---
+++ script.js
@@ -0,0 +1,46 @@
+const cards = document.querySelector(".cards");
+const images = document.querySelectorAll(".card__img");
+const backgrounds = document.querySelectorAll(".card__bg");
+const range = 40;
+
+
+window.addEventListener('deviceorientation', function(event) {
+    let beta = event.beta;
+    let gamma = event.gamma;
+  
+      cards.style.transform = `rotateX(${Math.floor(gamma)}deg) rotateY(${Math.floor(beta)}deg)`;
+  
+      [].forEach.call(images, (image) => {
+        image.style.transform = `translateX(${Math.floor(gamma)}px) translateY(${Math.floor(beta)}px)`;
+      });
+  
+      [].forEach.call(backgrounds, (background) => {
+        background.style.backgroundPosition = `${Math.floor(gamma)*45}px ${Math.floor(-beta)*.45}px`;
+      })
+
+}, false);
+
+// const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2)).toFixed(1);
+const calcValue = (a, b) => (a/b*range-range/2).toFixed(1) // thanks @alice-mx
+
+let timeout;
+document.addEventListener('mousemove', ({x, y}) => {
+  if (timeout) {
+    window.cancelAnimationFrame(timeout);
+  }
+  	
+  timeout = window.requestAnimationFrame(() => {
+    const yValue = calcValue(y, window.innerHeight);
+    const xValue = calcValue(x, window.innerWidth);
+
+    cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+
+    [].forEach.call(images, (image) => {
+      image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
+    });
+
+    [].forEach.call(backgrounds, (background) => {
+      background.style.backgroundPosition = `${xValue*.45}px ${-yValue*.45}px`;
+    })
+	})
+}, false);
